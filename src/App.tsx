3878ea278ffb7922d958e00d6a310a546f80228b
Tri Wulandari <112963736+twdtrii@users.noreply.github.com>
2025-04-05 16:09:12 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,21 +28,21 @@ export default function App() {
   }, [tasks])
 
   return (
-    <main className="flex justify-center items-center h-screen bg-gradient-to-r from-indigo-500 to-purple-600">
-      <section className="rounded-xl py-10 px-6 border border-white/30 bg-white/20 backdrop-blur-md shadow-xl w-[30rem]">
-        <h1 className="text-2xl font-semibold text-center text-white mb-2">Todo App</h1>
+    <main className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-300 to-pink-700">
+      <section className="rounded-xl py-10 px-6 border border-blue/100 bg-white/10 backdrop-blur-md shadow-xl w-[30rem]">
+        <h1 className="text-2xl font-semibold text-center text-white mb-2">Buat Tugas di To do List Website</h1>
         <div className="flex gap-2 mb-2 w-full">
           <input
             type="text"
             value={currentTask}
             onKeyDown={handleEnter}
             onChange={(e) => setCurrentTask(e.target.value)}
-            placeholder="Input your task"
+            placeholder="Masukan Tugas Anda"
             className="outline-none bg-white/20 rounded-md px-2 py-1 text-white/80 w-full"
           />
           <button
             onClick={handleAddTask}
-            className="border border-white/30 px-2 cursor-pointer rounded-md text-white bg-orange-400 hover:bg-orange-500"
+            className="border border-white/30 px-2 cursor-pointer rounded-md text-white bg-red-800 hover:bg-orange-500"
           >+</button>
         </div>
         <ul>
